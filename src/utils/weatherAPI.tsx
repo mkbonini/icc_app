068392b23dc00ca10fad9846429d23355439677a
mkbonini <mkbonini@gmail.com>
2023-03-30 17:02:38 -0600

    Create Weather Widget component to display api call data

--- src/utils/weatherAPI.tsx
+++ src/utils/weatherAPI.tsx
@@ -1,7 +1,7 @@
 export async function getWeather() {
 	try {
 		const response = await fetch(
-			`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=Denver&aqi=no`,
+			`http://api.weatherapi.com/v1/current.json?key=636cc7aa43b149be807194636233003&q=Denver&aqi=no`,
 			{
 				method: 'GET',
 				mode: 'cors',
